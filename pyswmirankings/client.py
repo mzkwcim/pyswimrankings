b322c6189659546fc7c6e994ea25a89c70279d0a
mzkwcim <wkrak98@gmail.com>
2025-12-29 21:46:19 +0100

    feat: implement athlete search request

--- pyswmirankings/client.py
+++ pyswmirankings/client.py
@@ -1,4 +1,9 @@
 import requsts
+from urllib.parse import urljoin
+from requests import HTTPError
+import logging
+
+logger = logging.getLogger(__name__)
 
 class SwimRankingsClient:
     def __init__(self, config: dict):
@@ -18,8 +23,43 @@ class SwimRankingsClient:
         gender: int | None = None,
         club_id: int | None = None
     ):
-        """Search for athletes"""
-        raise NotImplementedError
+        """
+        Search for athletes
+
+        Args:
+        gender:
+            Gender filter.
+            -1 = all
+             1 = male
+             2 = female
+        """
+        url = urljoin(self._base_url, "/index.php")
+        params = {
+            "internalRequest": "athleteFind",
+            "athlete_lastname": lastname,
+            "athlete_firstname": firstname,
+            "athlete_gender": gender,
+            "athlete_clubId": club_id,
+        }
+        filtered_params = {k: v for k, v in params.items() if v is not None}
+        response = self._session.get(
+            url,
+            params=filtered_params,
+            timeout=self._timeout,
+        )
+        try:
+            response.raise_for_status()
+        except HTTPError as exc:
+            logger.error(
+                "HTTP error during athlete search: status=%s url=%s params=%s",
+                response.status_code,
+                response.url,
+                filtered_params,
+                exc_info=exc
+            )
+            # TODO: handle rate limiting (429) and server errors (5xx)
+            raise
+        return response.text
 
     def search_clubs(
         self,
